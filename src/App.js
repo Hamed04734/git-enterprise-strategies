bbfd244cc9ad029a4c7f76f36611fd93619fc4dc
Hamed04734 <hamed0473@gmail.com>
2024-09-08 21:18:26 -0400

    Resolved Issue

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
       <header className="App-header">
         <img src={gatorLogo} className="App-logo" alt="logo" />
         <p> Hello World! </p>
+<<<<<<< Updated upstream
         <p> My name is Hamed Alwahadneh and this is my project </p>
+=======
+        <p> My name is ammar and this is my project </p>
+>>>>>>> Stashed changes
       </header>
     </div>
   );
